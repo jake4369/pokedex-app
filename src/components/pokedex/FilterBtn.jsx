39dc11839363ef679fdea372abde7a3e5624fba6
Jake Samuels <samuels242@googlemail.com>
2023-07-12 11:23:59 +0100

    Pokemon Filter button

--- src/components/pokedex/FilterBtn.jsx
+++ src/components/pokedex/FilterBtn.jsx
@@ -1,5 +1,9 @@
 const FilterBtn = ({ children, onFilterBtnClick }) => {
-  return <button onClick={onFilterBtnClick}>{children}</button>;
+  return (
+    <button onClick={onFilterBtnClick} className="pokedex-page__filter-btn">
+      {children}
+    </button>
+  );
 };
 
 export default FilterBtn;
