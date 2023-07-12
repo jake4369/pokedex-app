f68240a89e95098db047768e4daf98beccbd41d0
Jake Samuels <samuels242@googlemail.com>
2023-07-12 13:31:09 +0100

    Delete components that are not needed

--- src/components/pokedex/FilterBtn.jsx
+++
@@ -1,9 +0,0 @@
-const FilterBtn = ({ children, onFilterBtnClick }) => {
-  return (
-    <button onClick={onFilterBtnClick} className="pokedex-page__filter-btn">
-      {children}
-    </button>
-  );
-};
-
-export default FilterBtn;
